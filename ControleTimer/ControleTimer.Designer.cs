c9094e84d2cbca9fe467f4d0c0ae82bfd1d2dc77
Sebastien Michaud <msebastien1696@gmail.com>
2018-10-27 19:32:36 -0400

    Controle utilisateur representant un timer s'affichant sous forme textuelle

---
+++ ControleTimer/ControleTimer.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ControleTimer
+{
+    partial class ControleTimer
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(140, 112);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(33, 13);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "Timer";
+            // 
+            // ControleTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblTimer);
+            this.Name = "ControleTimer";
+            this.Size = new System.Drawing.Size(326, 234);
+            this.Load += new System.EventHandler(this.ControleTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
---
+++ ControleTimer/ControleTimer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+
+namespace ControleTimer
+{
+    public partial class ControleTimer: UserControl
+    {
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        public TimeSpan TInitial { get; set; }
+        private Thread thread;
+        private delegate void AfficherDelegue();
+        AfficherDelegue afficher;
+
+        public ControleTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void ControleTimer_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 1000;
+            lblTimer.Text = TInitial.ToString();
+        }
+
+        public void Demarrer()
+        {
+            afficher = new AfficherDelegue(AfficherTimerTxt);
+            thread = new Thread(new ThreadStart(CompteARebours));
+            thread.Start();
+        }
+
+        private void OnTimer_Tick(object sender, EventArgs e)
+        {
+            TInitial = TInitial.Subtract(new TimeSpan(0, 0, 1));
+            Invoke(afficher);
+        }
+
+        private void CompteARebours()
+        {
+            timer.Elapsed += new ElapsedEventHandler(OnTimer_Tick);
+            timer.Start();
+            Invoke(afficher);
+            while (Thread.CurrentThread.IsAlive)
+            {
+                if (TInitial.Equals(new TimeSpan(0, 0, 0)))
+                {
+                    timer.Stop();
+                    MessageBox.Show("Alerte");
+                    thread.Abort();
+                }
+            }
+        }
+
+        private void AfficherTimerTxt()
+        {            
+            lblTimer.Text = TInitial.ToString();                
+        }
+    }
+}
