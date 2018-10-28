7669158db177d5ce8d71a0ed23e6f658e40f2351
cyberced21 <32846628+cyberced21@users.noreply.github.com>
2018-10-28 19:34:18 -0400

    Revert "changement pour events"
    This reverts commit 66f902db981470041dde41e5a538bf6cbbd9429b.

--- ControleTimer/ControleTimer.cs
+++ ControleTimer/ControleTimer.cs
@@ -20,21 +20,9 @@ namespace ControleTimer
         private delegate void AfficherDelegue();
         AfficherDelegue afficher;
 
-
-        public delegate void TimerDoneEventHandler(Object source, EventArgs e);
-
-        public event TimerDoneEventHandler TimerDoneEvent;
-
         public ControleTimer()
         {
             InitializeComponent();
-        }
-
-        protected virtual void OnTimerDone()
-        {
-            //Si tu comprend pas check ste link la :
-            //https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-conditional-operators
-            TimerDoneEvent?.Invoke(this, EventArgs.Empty);
         }
 
         private void ControleTimer_Load(object sender, EventArgs e)
@@ -49,8 +37,6 @@ namespace ControleTimer
             thread = new Thread(new ThreadStart(CompteARebours));
             thread.Start();
         }
-
-       
 
         private void OnTimer_Tick(object sender, EventArgs e)
         {
@@ -68,7 +54,7 @@ namespace ControleTimer
                 if (TInitial.Equals(new TimeSpan(0, 0, 0)))
                 {
                     timer.Stop();
-                    OnTimerDone();//call the timer done method that lauches the timer done event
+                    MessageBox.Show("Alerte");
                     thread.Abort();
                 }
             }
--- test/Form1.cs
+++ test/Form1.cs
@@ -15,16 +15,11 @@ namespace test
         public Form1()
         {
             InitializeComponent();
-            controle = new ControleTime();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             this.controleTimer1.Demarrer();
-        }
-        private void OnTimerDone(Object Source, EventArgs e)
-        {
-            MessageBox.Show("Timer fini");
         }
     }
 }
